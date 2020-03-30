0799744d4cd29424eb6348d93f2a3480499a7b3f
Wouter Beeftink <wouter.beeftink@gmail.com>
2020-03-30 21:11:37 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// tslint:disable: no-console
+
+import { from } from "rxjs";
+import { map } from "rxjs/operators";
+
+const people: string[] = ["Micheal", "Jim", "Dwight"];
+
+from(people)
+	.pipe(map(name => `${name} says hi!`))
+	.subscribe(message => console.log(message));
